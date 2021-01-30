db04edb90fe1df57f98324fcece43bb582f089a7
Gweivyth Quartet <admin@mageringnetwork.com>
2021-01-30 17:13:53 +0000

    Utsusemi: Ichi removes Third Eye.

--- scripts/globals/spells/utsusemi_ichi.lua
+++ scripts/globals/spells/utsusemi_ichi.lua
@@ -5,26 +5,27 @@ require("scripts/globals/status")
 require("scripts/globals/msg")
 -----------------------------------------
 
-function onMagicCastingCheck(caster, target, spell)
+function onMagicCastingCheck(caster,target,spell)
     return 0
 end
 
-function onSpellCast(caster, target, spell)
+function onSpellCast(caster,target,spell)
     local effect = target:getStatusEffect(tpz.effect.COPY_IMAGE)
+	and target:delStatusEffect(67) -- Third Eye and Utsusemi don't stack.  Utsusemi removes Third Eye.
 
     -- Get extras shadows
-    local numShadows = 3 + target:getMod(tpz.mod.UTSUSEMI_BONUS)
-    local icon = tpz.effect.COPY_IMAGE_3
+    local numShadows = 3 + target:getMod(dsp.mod.UTSUSEMI_BONUS)
+    local icon = dsp.effect.COPY_IMAGE_3
     if (numShadows > 3) then
-        icon = tpz.effect.COPY_IMAGE_4
+        icon = dsp.effect.COPY_IMAGE_4
     end
-
+    
     if (effect == nil or effect:getPower() <= 1) then
-        target:addStatusEffectEx(tpz.effect.COPY_IMAGE, icon, 1, 0, 900, 0, numShadows)
-        spell:setMsg(tpz.msg.basic.MAGIC_GAIN_EFFECT)
+        target:addStatusEffectEx(dsp.effect.COPY_IMAGE, icon, 1, 0, 900, 0, numShadows)
+        spell:setMsg(dsp.msg.basic.MAGIC_GAIN_EFFECT)
     else
-        spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
+        spell:setMsg(dsp.msg.basic.MAGIC_NO_EFFECT)
     end
 
-    return tpz.effect.COPY_IMAGE
+    return dsp.effect.COPY_IMAGE
 end
