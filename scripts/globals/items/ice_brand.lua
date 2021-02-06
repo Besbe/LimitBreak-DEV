2848793f5a2ad508a4c8c1b579c2bd7ec3f67041
gweivyth <37130689+gweivyth@users.noreply.github.com>
2021-02-06 18:06:30 -0500

    Adds additional effects to almost 100 weapons.

--- scripts/globals/items/ice_brand.lua
+++ scripts/globals/items/ice_brand.lua
@@ -1,7 +1,8 @@
 -----------------------------------------
 -- ID: 16937
--- Ice Brand
+-- Item: Ice Brand
 -- Additional Effect: Ice Damage
+-- Gweivyth
 -----------------------------------------
 require("scripts/globals/status")
 require("scripts/globals/magic")
@@ -9,12 +10,12 @@ require("scripts/globals/msg")
 -----------------------------------
 
 function onAdditionalEffect(player,target,damage)
-    local chance = 10
+    local chance = 15
 
     if (math.random(0,99) >= chance) then
         return 0,0,0
     else
-        local dmg = math.random(7,21)
+        local dmg = math.random(13,26)
         local params = {}
         params.bonusmab = 0
         params.includemab = false
